db6a9d49b346626c608cb496290bdb87dcfb3ed4
Ђорђе <37450617+Djolevil@users.noreply.github.com>
2021-10-10 18:30:02 +0200

    Use Redirect component by react router to redirect user and finish project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
@@ -10,16 +10,19 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
-      <Route path='/welcome'>
-        <Welcome />
-      </Route>
-      <Route path='/products' exact>
-        <Products />
-      </Route>
-      <Route path='/products/:productId'>
-        <ProductDetail />
-      </Route>
-      </Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
